26e4a983f020ebcdb4060b63f8ae9c58771820d8
ali-bonaftizadeh <alibnz.2001@gmail.com>
2024-03-11 11:41:28 +0330

    fix a bug in return book

--- library/src/main/classes/Library.java
+++ library/src/main/classes/Library.java
@@ -60,6 +60,7 @@ public class Library {
         }
         if (student.hasBook(book)) {
             this.books.add(book);
+            student.removeBook(book);
             System.out.println(student.getName() + " returned " + book.getTitle() + ".");
             return true;
         }
