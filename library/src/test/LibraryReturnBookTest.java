936ab6d658fc32abbf82899486613b669abe755e
ali-bonaftizadeh <alibnz.2001@gmail.com>
2024-03-11 11:01:11 +0330

    add a test for found bug

---
+++ library/src/test/LibraryReturnBookTest.java
@@ -0,0 +1,32 @@
+package test;
+
+
+import main.classes.Book;
+import main.classes.Library;
+import main.classes.SearchByType;
+import main.classes.Student;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryReturnBookTest {
+    @Test
+    public void returnBookTest(){
+        //Arrange
+        final Library library = new Library();
+        final Book book = new Book("title", "mmd", 2);
+        final Student ali = new Student("ali", 1);
+
+        //Act
+        library.addBook(book);
+        library.lendBook(book, ali);
+        library.returnBook(book, ali);
+
+        //Assert
+        final ArrayList<Object> searchList = new ArrayList<>();
+        searchList.add(2);
+        Assert.assertEquals(library.searchBooks(SearchByType.ID, searchList), null);
+    }
+}
